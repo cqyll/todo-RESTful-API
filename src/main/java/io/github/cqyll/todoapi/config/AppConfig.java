45b2aea634de19f06806987eb985210786b8cdc6
cqyll <97649063+cqyll@users.noreply.github.com>
2026-01-02 16:16:36 -0500

    Set up HTTP server and POST /register endpoint in AppConfig
    Added createHttpServer() to create and configure the HTTP server using
    localhost on port 8080. Requests are routed by URL path, and HTTP method
    validation is left to corresponding controller.

--- src/main/java/io/github/cqyll/todoapi/config/AppConfig.java
+++ src/main/java/io/github/cqyll/todoapi/config/AppConfig.java
@@ -4,36 +4,62 @@ import io.github.cqyll.todoapi.adapter.inbound.web.UserController;
 import io.github.cqyll.todoapi.adapter.inbound.web.AuthController;
 import io.github.cqyll.todoapi.application.service.UserRegistrationService;
 import io.github.cqyll.todoapi.application.service.UserAuthenticationService;
+import io.github.cqyll.todoapi.application.port.outbound.UserRepositoryPort;
+import io.github.cqyll.todoapi.application.port.outbound.PasswordHasherPort;
+import io.github.cqyll.todoapi.application.port.outbound.TokenProviderPort;
 import io.github.cqyll.todoapi.adapter.outbound.persistence.InMemoryUserAdapter;
 import io.github.cqyll.todoapi.adapter.outbound.security.SimplePasswordHasherAdapter;
 import io.github.cqyll.todoapi.adapter.outbound.security.FakeTokenProviderAdapter;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpContext;
+
+
 public class AppConfig {
-    private UserRegistrationService userRegistrationService;
-    private UserAuthenticationService userAuthenticationService;
-    private UserController userController;
-    private AuthController authController;
-    
-    public AppConfig() {
-        initialize();
-    }
-    
-    private void initialize() {
-        // created adapters
-        var userRepository = new InMemoryUserAdapter();
-        var passwordHasher = new SimplePasswordHasherAdapter();
-        var tokenProvider = new FakeTokenProviderAdapter();
-        
-        // created services for each use case
-        userRegistrationService = new UserRegistrationService(userRepository, passwordHasher);
-        userAuthenticationService = new UserAuthenticationService(
-            userRepository, passwordHasher, tokenProvider);
-        
-        // created corresponding controllers
-        userController = new UserController(userRegistrationService);
-        authController = new AuthController(userAuthenticationService);
-    }
-    
-    public UserController getUserController() { return userController; }
-    public AuthController getAuthController() { return authController; }
+	private UserRegistrationService userRegistrationService;
+	private UserAuthenticationService userAuthenticationService;
+	private UserController userController;
+	private AuthController authController;
+
+	public AppConfig() {
+		initialize();
+	}
+
+	private void initialize() {
+		// created adapters
+		UserRepositoryPort userRepository = new InMemoryUserAdapter();
+		PasswordHasherPort passwordHasher = new SimplePasswordHasherAdapter();
+		TokenProviderPort tokenProvider = new FakeTokenProviderAdapter();
+
+		// created services for each use case
+		userRegistrationService = new UserRegistrationService(userRepository, passwordHasher);
+		// userAuthenticationService = new UserAuthenticationService(
+		//    userRepository, passwordHasher, tokenProvider);
+
+		// created corresponding controllers
+		userController = new UserController(userRegistrationService);
+		// authController = new AuthController(userAuthenticationService);
+
+	}
+
+	public HttpServer createHttpServer() {
+		try {
+			// creating socket address via hostname string may resolve to IPv6
+			HttpServer server = HttpServer.create(
+					new InetSocketAddress("localhost", 8080), 0);
+
+			server.createContext("/register", userController);
+
+			return server;
+		} catch(IOException e) {
+			throw new RuntimeException("Failed to create HTTP server", e);
+		}
+	}
+
+	public UserController getUserController() { return userController; }
+	public AuthController getAuthController() { return authController; }
 }
